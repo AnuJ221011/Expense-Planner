027618c736de2d985bdfd227c7471a29e979de0f
Anuj Kumar <anuj.sot010022@pwioi.com>
2025-06-11 02:29:54 +0530

    removed extas endpoints

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,8 @@ const { initDB } = require('./db');
 const balanceRoutes = require('./routes/balanceRoutes');
 const transactionRoutes = require('./routes/transactions');
 const savingsRoutes = require('./routes/savings');
-const recentTransactionRoutes = require('./routes/recentTransaction'); // Adjust path as needed
-const analyticsRoutes = require('./routes/analytics'); // Adjust path
-
+const recentTransactionRoutes = require('./routes/recentTransaction'); 
+const analyticsRoutes = require('./routes/analytics'); 
 
 
 // Initialize the database
@@ -22,7 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/routes/auth', authRoutes);
 app.use('/api/balance', balanceRoutes);
-app.use('/api', transactionRoutes); // now handles /api/income and /api/expense
+app.use('/api', transactionRoutes); 
 app.use('/api/savings', savingsRoutes);
 app.use('/api', recentTransactionRoutes);
 app.use('/api', analyticsRoutes);
